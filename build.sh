6af0e6de1281c31beb09e532b1a4806d983f3916
MrToxicBat <118140168+MrToxicBat@users.noreply.github.com>
2025-05-03 18:26:37 -0400

    build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-pip install --upgrade pip
-pip install --upgrade google-generativeai==0.3.2
+pip install --upgrade pip setuptools wheel
+pip uninstall -y google-generativeai google-ai-generativelanguage || true
+pip install google-generativeai==0.3.2
