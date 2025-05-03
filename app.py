c1d9b1d99959bbcbda9ce82540b78c20698a3d07
MrToxicBat <118140168+MrToxicBat@users.noreply.github.com>
2025-05-03 15:34:59 -0400

    app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import google.generativeai as genai
+import os
 
-genai.configure(api_key="TU_API_KEY_DE_GEMINI")
+# Cargar la API Key desde variables de entorno
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
 app = Flask(__name__)
 CORS(app)
